1929b22662f2d0a4dfbd3da8506c0d30cc66d745
Madina <madinatussupova0820@gmail.com>
2022-10-13 15:46:53 +0600

    save jobs to local storage

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const btnInput = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const listEl = document.querySelector("#ul-el");
 
-const myJobs = [];
+let myJobs = [];
+
+localStorage.clear();
+let jobsFromLocalStorage = JSON.parse(localStorage.getItem("myJobs"));
+
+console.log(jobsFromLocalStorage);
 
 // Add jobs to array of jobs
 btnInput.addEventListener("click", () => {
   if (!myJobs.includes(inputEl.value)) {
     myJobs.push(inputEl.value);
     inputEl.value = "";
+
+    // Save jobs to locak storage
+    localStorage.setItem("myJobs", JSON.stringify(myJobs));
+
     renderJobs();
   } else return;
   console.log(myJobs);
 });
+
 
 // Display jobs in the list
 function renderJobs() {
@@ -22,3 +32,15 @@ function renderJobs() {
   }
   listEl.innerHTML = listItems;
 }
+
+
+
+
+// myJobs = JSON.parse(myJobs);
+// myJobs.push("job1");
+// myJobs = JSON.stringify(myJobs);
+// console.log(typeof myJobs);
+
+// localStorage.setItem("myJobs", "www.examplejob.com");
+// console.log(localStorage.getItem("myJobs"));
+// localStorage.clear()
