e13efe455f607bb0de00b9a98cf308ead4c5a650
Madina <madinatussupova0820@gmail.com>
2022-10-24 16:50:17 +0600

    remove job from list

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const listEl = document.querySelector("#ul-el");
 
 let myJobs = [];
 
-localStorage.clear();
+// localStorage.clear();
 let jobsFromLocalStorage = JSON.parse(localStorage.getItem("myJobs"));
 
 console.log(jobsFromLocalStorage);
@@ -28,9 +28,19 @@ btnInput.addEventListener("click", () => {
 function renderJobs() {
   let listItems = "";
   for (let job of myJobs) {
-    listItems += `<li><a href="${job}" target="_blank">${job}</a></li>`;
+    let listItem = `<li class="job-item"><a href="${job}" target="_blank">${job}</a><a onclick="delJob()"><img src="./btn-del.png" class="del-btn" alt="delete button"></a></li>`;
+    listItems += listItem;
   }
+  let doc = new DOMParser().parseFromString(listItems, 'text/html');
+  console.log(doc);
+
   listEl.innerHTML = listItems;
+}
+
+
+function delJob() {
+  let job = document.querySelector('.job-item');
+  job.remove();
 }
 
 
