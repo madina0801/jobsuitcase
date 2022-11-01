4e538803792b2b0e9382f42e16e91bfa379c6eb2
Madina <madinatussupova0820@gmail.com>
2022-11-01 15:00:00 +0600

    delete all jobs

--- index.js
+++ index.js
@@ -1,56 +1,43 @@
 const btnInput = document.querySelector("#input-btn");
+const btnDel = document.querySelector("#del-btn");
 const inputEl = document.querySelector("#input-el");
 const listEl = document.querySelector("#ul-el");
 
 let myJobs = [];
 
-// localStorage.clear();
 let jobsFromLocalStorage = JSON.parse(localStorage.getItem("myJobs"));
 
-console.log(jobsFromLocalStorage);
+if(jobsFromLocalStorage) {
+  myJobs = jobsFromLocalStorage;
+  renderJobs();
+}
 
 // Add jobs to array of jobs
 btnInput.addEventListener("click", () => {
-  if (!myJobs.includes(inputEl.value)) {
-    myJobs.push(inputEl.value);
-    inputEl.value = "";
+  myJobs.push(inputEl.value);
+  inputEl.value = "";
 
-    // Save jobs to locak storage
-    localStorage.setItem("myJobs", JSON.stringify(myJobs));
-
-    renderJobs();
-  } else return;
+  // Save jobs to local storage
+  localStorage.setItem("myJobs", JSON.stringify(myJobs));
+  renderJobs();
   console.log(myJobs);
 });
 
 
-// Display jobs in the list
+// Render jobs in the list
 function renderJobs() {
   let listItems = "";
-  for (let job of myJobs) {
-    let listItem = `<li class="job-item"><a href="${job}" target="_blank">${job}</a><a onclick="delJob()"><img src="./btn-del.png" class="del-btn" alt="delete button"></a></li>`;
-    listItems += listItem;
+  for (let i = 0; i < myJobs.length; i++) {
+    listItems += `<li class="job-item"><a href="${myJobs[i]}" target="_blank">${myJobs[i]}</a></li>`;
   }
-  let doc = new DOMParser().parseFromString(listItems, 'text/html');
-  console.log(doc);
-
   listEl.innerHTML = listItems;
 }
 
-
-function delJob() {
-  let job = document.querySelector('.job-item');
-  job.remove();
+// Delete all jobs
+function delJobs() {
+  localStorage.clear();
+  myJobs = [];
+  listEl.innerHTML = "";
 }
 
-
-
-
-// myJobs = JSON.parse(myJobs);
-// myJobs.push("job1");
-// myJobs = JSON.stringify(myJobs);
-// console.log(typeof myJobs);
-
-// localStorage.setItem("myJobs", "www.examplejob.com");
-// console.log(localStorage.getItem("myJobs"));
-// localStorage.clear()
+btnDel.addEventListener("dblclick", delJobs);
